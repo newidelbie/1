edd552fbfe4bc3d243d3883c28fe0871cac03098
newidelbie <149589477+newidelbie@users.noreply.github.com>
2024-01-23 13:56:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ client.on('interactionCreate', async (interaction) => {
   }
 });
 
-client.on('messageCreate', async message => {
+client.on('message', async message => {
   if(message.content === 'ip') {
     message.channel.send(`
     MegaMC
